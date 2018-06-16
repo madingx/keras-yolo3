d932c5c8d2449787cfc13d6a1e7017a39ad160d5
madingx <mading@bjtu.edu.cn>
2018-06-16 21:44:33 +0800

    计算IOU

--- view.py
+++ view.py
@@ -2,6 +2,7 @@
 #coding=utf-8
 import cv2
 import numpy as np
+import xml.etree.ElementTree as ET
 
 def draw_result(img, result):
     for i in range(len(result)):
@@ -36,10 +37,96 @@ def read_result_file(path):
             print(key)
             if key==27:
                 break
-            
+
+
+def calcu_IOU(Reframe,GTframe):
+    """
+    自定义函数，计算两矩形 IOU，传入为均为矩形对角线，（x,y）  坐标。
+    """
+    x1 = Reframe[0]
+    y1 = Reframe[1]
+    width1 = Reframe[2]-Reframe[0]
+    height1 = Reframe[3]-Reframe[1]
+
+    x2 = GTframe[0]
+    y2 = GTframe[1]
+    width2 = GTframe[2]-GTframe[0]
+    height2 = GTframe[3]-GTframe[1]
+
+    endx = max(x1+width1,x2+width2)
+    startx = min(x1,x2)
+    width = width1+width2-(endx-startx)
+
+    endy = max(y1+height1,y2+height2)
+    starty = min(y1,y2)
+    height = height1+height2-(endy-starty)
+
+    if width <=0 or height <= 0:
+        ratio = 0 # 重叠率为 0 
+    else:
+        Area = width*height # 两矩形相交面积
+        Area1 = width1*height1
+        Area2 = width2*height2
+        ratio = Area*1./(Area1+Area2-Area)
+    # return IOU
+    return ratio,Reframe,GTframe
+
+VOCpath = "Satellite"    
+classes = ["airport", "bridge", "harbor"]
+myIOU = 0.7
+def calcu_F1():
+    with open(path,'r') as f:
+        TP = 0
+        FP = 0
+        TN = 0
+        FN = 0
+
+
+        for lines in f.readlines():
+            line_arr = lines.split(',')
+            imgname = line_arr[0]
+            preclass = line_arr[1]
+            prebox = line_arr[0].split(' ')   
+            prexy = [int(prebox[0]),int(prebox[1]),int(prebox[2])+int(prebox[0]),int(prebox[3])+int(prebox[1])]  
+
+            in_file = open(VOCpath+'/Annotations/%s.xml'%(imgname[:-4]))
+            tree=ET.parse(in_file)
+            root = tree.getroot()
+            for obj in root.iter('object'):
+                difficult = obj.find('difficult').text
+                cls = obj.find('name').text
+                if cls not in classes or int(difficult)==1:
+                    continue
+                if cls != preclass:
+                    continue
+
+                xmlbox = obj.find('bndbox')
+                b = [int(xmlbox.find('xmin').text), int(xmlbox.find('ymin').text), int(xmlbox.find('xmax').text), int(xmlbox.find('ymax').text)]
+
+                if calcu_IOU(b,prexy) > myIOU:
+                    TP += 1
+                else:
+                    FP += 1
+
+        xml_names = sorted(os.listdir(VOCpath+'/Annotations/'))
+        Tcnt = 0
+        for i in xml_names:
+            if i[:-3] != 'xml':continue
+            in_file = open(VOCpath+'/Annotations/'+i)
+            tree=ET.parse(in_file)
+            root = tree.getroot()
+            Tcnt += len(root.findall('object'))
+
+        FN = Tcnt - TP
+        scoreR = TP/(TP+FN)
+        scoreP = TP/(TP+FP)
+        scoreF1 = 2.0*scoreP*scoreR/(scoreR+scoreP)
+        print(scoreF1)
+        return 
+
 
 if __name__ == '__main__':
     path = 'SSD84271result.txt'
-    read_result_file(path)
+    calcu_F1(path)
     
             
